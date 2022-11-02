2414384abfbaa72b0381b912a198218d8f5d7bcf
Reemanshi <reema07chugh@gmail.com>
2022-11-02 10:02:20 +0530

    feat: added firebase config for hoisting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const withImages = require("next-images");
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // ...withImages(),
+};
