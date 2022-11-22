d828beb1bf61a46f328e137ee1faa216aadac332
Reemanshi <reema07chugh@gmail.com>
2022-11-22 12:39:55 +0530

    fix: minor change

--- components/headers/light.js
+++ components/headers/light.js
@@ -1,120 +1,29 @@
-import MenuIcon from "feather-icons/dist/icons/menu.svg";
-import CloseIcon from "feather-icons/dist/icons/x.svg";
-import { motion } from "framer-motion";
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import tw from "twin.macro";
 import LogoSVG from "../../assets/images/logo.svg";
-import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
 import useWindowDimensions from "../../helpers/getWindowDimensions";
 
 const Header = tw.header`
   flex justify-center items-center
-  fixed bg-white top-0 w-full z-20 p-8 md:px-24 2xl:p-8 mx-auto w-full
+  fixed bg-white top-0 w-full z-20 p-4 md:px-24 2xl:p-8 mx-auto w-full
 `;
 
-export const NavLinks = tw.div`flex flex-col items-center lg:flex-row lg:inline-block`;
-
-/* hocus: stands for "on hover or focus"
- * hocus:bg-primary-700 will apply the bg-primary-700 class on hover or focus
- */
-export const NavLink = tw.a`
-  text-lg my-2 lg:text-sm lg:mx-6 lg:my-0
-  font-semibold tracking-wide transition duration-300
-  pb-1 border-b-2 border-transparent hover:border-primary-500 hocus:text-primary-500
-`;
-
-export const PrimaryLink = tw(NavLink)`
-  lg:mx-0
-  px-8 py-3 rounded bg-primary-500 text-gray-100
-  hocus:bg-secondary-700 hocus:text-gray-200 focus:shadow-outline
-  border-b-0
-`;
-
-export const LogoLink = styled(NavLink)`
-  ${tw`flex items-center font-black border-b-0 text-2xl! ml-0!`};
-
-  img {
-    ${tw`w-10 mr-3`}
-  }
-`;
-
-export const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
-export const NavToggle = tw.button`
-  lg:hidden z-20 focus:outline-none hocus:text-primary-500 transition duration-300
-`;
-export const MobileNavLinks = motion(styled.div`
-  ${tw`lg:hidden z-10 fixed top-0 inset-x-0 mx-4 my-6 p-8 border text-center rounded-lg text-gray-900 bg-white`}
-  ${NavLinks} {
-    ${tw`flex flex-col items-center`}
-  }
-`);
-
-export const DesktopNavLinks = tw.nav`
-  hidden lg:flex flex-1 justify-between items-center max-w-screen-xl
-`;
-
-export default ({
-  roundedHeaderButton = false,
-  logoLink,
-  links,
-  className,
-  collapseBreakpointClass = "lg",
-}) => {
+export default ({ logoLink, className }) => {
   const { width } = useWindowDimensions();
   useEffect(() => {
     width > 768 && setIsToggle(false);
   }, [width]);
-
-  /*
-   * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
-   * This links props should be an array of "NavLinks" components which is exported from this file.
-   * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
-   * This allows this Header to be multi column.
-   * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
-   * Left part will be LogoLink, and the right part will be the the NavLinks component you
-   * supplied.
-   * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
-   * You can also choose to directly modify the links here by not passing any links from the parent component and
-   * changing the defaultLinks variable below below.
-   * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
-   */
-  const defaultLinks = [
-    <NavLinks key={1}>
-      <NavLink href="/about-us">About</NavLink>
-      <NavLink href="/blog-post">Blog</NavLink>
-      <PrimaryLink
-        css={roundedHeaderButton && tw`rounded-full`}
-        href="/contact-us"
-      >
-        Contact Us
-      </PrimaryLink>
-      {/* <NavLink href="/#" tw="lg:ml-12!">
-        Login
-      </NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`} href="/#">
-        Sign Up
-      </PrimaryLink> */}
-    </NavLinks>,
-  ];
-
-  const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
-  const collapseBreakpointCss =
-    collapseBreakPointCssMap[collapseBreakpointClass];
 
   const defaultLogoLink = (
     <a
       className="my-2 lg:text-sm lg:my-0 tracking-wide transition duration-300 pb-1 border-transparent hover:border-primary-500 hocus:text-primary-500 flex items-center font-black border-b-0 xl:text-2xl ml-0!"
       href="/"
     >
-      {/* <Logo alt="logo" className="mr-3" /> */}
-      {/* <img src={logo.src} /> */}
       <LogoSVG alt="logo" className="mr-3" />
     </a>
   );
 
   logoLink = logoLink || defaultLogoLink;
-  links = links || defaultLinks;
 
   const [isToggle, setIsToggle] = useState(false);
 
@@ -124,36 +33,6 @@ export default ({
 
   return (
     <>
-      {/* <Header className={className || "header-light"}>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
-        {logoLink}
-        {links}
-      </DesktopNavLinks>
-
-      <MobileNavLinksContainer
-        css={collapseBreakpointCss.mobileNavLinksContainer}
-      >
-        {logoLink}
-        <MobileNavLinks
-          initial={{ x: "150%", display: "none" }}
-          animate={animation}
-          css={collapseBreakpointCss.mobileNavLinks}
-        >
-          {links}
-        </MobileNavLinks>
-        <NavToggle
-          onClick={toggleNavbar}
-          className={showNavLinks ? "open" : "closed"}
-        >
-          {showNavLinks ? (
-            <CloseIcon className="w-6 h-6" />
-          ) : (
-            <MenuIcon className="w-6 h-6" />
-          )}
-        </NavToggle>
-      </MobileNavLinksContainer>
-    </Header> */}
-
       <Header className={className || "header-light"}>
         <div
           className={
@@ -259,33 +138,4 @@ export default ({
       </Header>
     </>
   );
-};
-
-/* The below code is for generating dynamic break points for navbar.
- * Using this you can specify if you want to switch
- * to the toggleable mobile navbar at "sm", "md" or "lg" or "xl" above using the collapseBreakpointClass prop
- * Its written like this because we are using macros and we can not insert dynamic variables in macros
- */
-
-const collapseBreakPointCssMap = {
-  sm: {
-    mobileNavLinks: tw`sm:hidden`,
-    desktopNavLinks: tw`sm:flex`,
-    mobileNavLinksContainer: tw`sm:hidden`,
-  },
-  md: {
-    mobileNavLinks: tw`md:hidden`,
-    desktopNavLinks: tw`md:flex`,
-    mobileNavLinksContainer: tw`md:hidden`,
-  },
-  lg: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`,
-  },
-  xl: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`,
-  },
 };
