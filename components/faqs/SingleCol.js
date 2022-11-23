3f5af1732448efe98c420bed7638a9647ab0bfe0
Reemanshi <reema07chugh@gmail.com>
2022-11-23 11:13:13 +0530

    fix: collaborate modal

--- components/faqs/SingleCol.js
+++ components/faqs/SingleCol.js
@@ -1,10 +1,10 @@
-import ChevronDownIcon from "feather-icons/dist/icons/chevron-down.svg"
-import { motion } from "framer-motion"
-import React, { useState } from "react"
-import styled from "styled-components"
-import tw from "twin.macro"
-import SvgDecoratorBlob1 from "../../assets/images/svg-decorator-blob-7.svg"
-import SvgDecoratorBlob2 from "../../assets/images/svg-decorator-blob-8.svg"
+import ChevronDownIcon from 'feather-icons/dist/icons/chevron-down.svg';
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import SvgDecoratorBlob1 from '../../assets/images/svg-decorator-blob-7.svg';
+import SvgDecoratorBlob2 from '../../assets/images/svg-decorator-blob-8.svg';
 
 // const Subheading = tw(SubheadingBase)`mb-4 text-center`;
 // const Heading = tw(SectionHeading)`w-full`;
@@ -22,10 +22,10 @@ const QuestionToggleIcon = motion(styled.span`
   svg {
     ${tw`w-6 h-6`}
   }
-`)
+`);
 const Answer = motion(
   tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`
-)
+);
 
 // const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
 //   ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400`}
@@ -35,38 +35,38 @@ const Answer = motion(
 // `;
 
 export default ({
-  subheading = "FAQS",
-  heading = "You have Questions ?",
-  description = "Here are some common questions that our clients come up with before signing up with us.",
+  subheading = 'FAQS',
+  heading = 'You have Questions ?',
+  description = 'Here are some common questions that our clients come up with before signing up with us.',
   faqs = [
     {
-      question: "Is lunch provided free of cost ?",
+      question: 'Is lunch provided free of cost ?',
       answer:
-        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system.",
+        'Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system.',
     },
     {
-      question: "Do you have 2 Bedroom suites ?",
+      question: 'Do you have 2 Bedroom suites ?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      question: "Are Wi-Fi costs included in the price ?",
+      question: 'Are Wi-Fi costs included in the price ?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      question: "Where can I reach you for support ?",
+      question: 'Where can I reach you for support ?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
   ],
 }) => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null)
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
 
   const toggleQuestion = (questionIndex) => {
-    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null)
-    else setActiveQuestionIndex(questionIndex)
-  }
+    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
+    else setActiveQuestionIndex(questionIndex);
+  };
 
   return (
     <div className="relative" id="faq">
@@ -93,7 +93,7 @@ export default ({
                 className="cursor-pointer select-none mt-5 px-8 sm:px-10 py-5 sm:py-4 rounded-lg text-gray-800 hover:text-gray-900 bg-gray-200 hover:bg-gray-300 transition duration-300"
                 key={index}
                 onClick={() => {
-                  toggleQuestion(index)
+                  toggleQuestion(index);
                 }}
                 // className="group"
               >
@@ -108,7 +108,7 @@ export default ({
                     }}
                     initial="collapsed"
                     animate={
-                      activeQuestionIndex === index ? "open" : "collapsed"
+                      activeQuestionIndex === index ? 'open' : 'collapsed'
                     }
                     transition={{
                       duration: 0.02,
@@ -121,11 +121,11 @@ export default ({
                 <Answer
                   // className="pointer-events-none text-sm sm:text-base leading-relaxed"
                   variants={{
-                    open: { opacity: 1, height: "auto", marginTop: "16px" },
-                    collapsed: { opacity: 0, height: 0, marginTop: "0px" },
+                    open: { opacity: 1, height: 'auto', marginTop: '16px' },
+                    collapsed: { opacity: 0, height: 0, marginTop: '0px' },
                   }}
                   initial="collapsed"
-                  animate={activeQuestionIndex === index ? "open" : "collapsed"}
+                  animate={activeQuestionIndex === index ? 'open' : 'collapsed'}
                   transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
                 >
                   {faq.answer}
@@ -138,5 +138,5 @@ export default ({
       <SvgDecoratorBlob1 className="pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400" />
       <SvgDecoratorBlob2 className="pointer-events-none -z-20 absolute left-0 bottom-0 h-64 w-64 opacity-15 transform -translate-x-2/3 text-primary-500" />
     </div>
-  )
-}
+  );
+};
--- pages/SaaSProductLandingPage.js
+++ pages/SaaSProductLandingPage.js
@@ -1,23 +1,23 @@
-import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
-import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
-import React from "react";
-import tw from "twin.macro";
-import heroScreenshotImageSrc from "../assets/images/hero-screenshot-1.png";
-import macHeroScreenshotImageSrc from "../assets/images/hero-screenshot-2.png";
-import prototypeIllustrationImageSrc from "../assets/images/prototype-illustration.svg";
-import GetStarted from "../components/cta/GetStarted";
-import FAQ from "../components/faqs/SingleCol.js";
-import Features from "../components/features/ThreeColWithSideImage.js";
-import MainFeature from "../components/features/TwoColWithButton.js";
-import FeatureWithSteps from "../components/features/TwoColWithSteps.js";
-import MainFeature2 from "../components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-import Footer from "../components/footers/FiveColumnWithBackground.js";
+import { ReactComponent as BriefcaseIcon } from 'feather-icons/dist/icons/briefcase.svg';
+import { ReactComponent as MoneyIcon } from 'feather-icons/dist/icons/dollar-sign.svg';
+import React from 'react';
+import tw from 'twin.macro';
+import heroScreenshotImageSrc from '../assets/images/hero-screenshot-1.png';
+import macHeroScreenshotImageSrc from '../assets/images/hero-screenshot-2.png';
+import prototypeIllustrationImageSrc from '../assets/images/prototype-illustration.svg';
+import GetStarted from '../components/cta/GetStarted';
+import FAQ from '../components/faqs/SingleCol.js';
+import Features from '../components/features/ThreeColWithSideImage.js';
+import MainFeature from '../components/features/TwoColWithButton.js';
+import FeatureWithSteps from '../components/features/TwoColWithSteps.js';
+import MainFeature2 from '../components/features/TwoColWithTwoHorizontalFeaturesAndButton.js';
+import Footer from '../components/footers/FiveColumnWithBackground.js';
 // import { css } from "styled-components/macro"; //eslint-disable-line
-import Hero from "../components/hero/TwoColumnWithInput.js";
-import Pricing from "../components/pricing/ThreePlans.js";
-import Testimonial from "../components/testimonials/TwoColumnWithImageAndRating.js";
-import Header from "../components/headers/light";
-import Head from "next/head";
+import Hero from '../components/hero/TwoColumnWithInput.js';
+import Pricing from '../components/pricing/ThreePlans.js';
+import Testimonial from '../components/testimonials/TwoColumnWithImageAndRating.js';
+import Header from '../components/headers/light';
+import Head from 'next/head';
 
 const SaaSProductLandingPage = () => {
   return (
@@ -66,7 +66,7 @@ const SaaSProductLandingPage = () => {
         <FeatureWithSteps
           subheading={
             <span className="uppercase tracking-widest font-bold text-primary-500">
-              {" "}
+              {' '}
               <>
                 Take the easy <span className="text-primary-500">way out</span>
               </>
@@ -90,16 +90,16 @@ const SaaSProductLandingPage = () => {
           features={[
             {
               Icon: MoneyIcon,
-              title: "Affordable",
+              title: 'Affordable',
               description:
-                "We promise to offer you the best rate we can - at par with the industry standard.",
+                'We promise to offer you the best rate we can - at par with the industry standard.',
               iconContainerCss: tw`bg-green-300 text-green-800`,
             },
             {
               Icon: BriefcaseIcon,
-              title: "Professionalism",
+              title: 'Professionalism',
               description:
-                "We assure you that our templates are designed and created by professional designers.",
+                'We assure you that our templates are designed and created by professional designers.',
               iconContainerCss: tw`bg-red-300 text-red-800`,
             },
           ]}
@@ -112,46 +112,46 @@ const SaaSProductLandingPage = () => {
           }
           heading={
             <>
-              Find the best Developers{" "}
+              Find the best Developers{' '}
               <span className="text-primary-500">here.</span>
             </>
           }
           plans={[
             {
-              name: "Technology",
-              price: "$17.99",
-              duration: "Monthly",
-              mainFeature: "Individuals",
+              name: 'Technology',
+              price: '$17.99',
+              duration: 'Monthly',
+              mainFeature: 'Individuals',
               features: [
-                "30 Templates",
-                "7 Landing Pages",
-                "12 Internal Pages",
-                "Basic Assistance",
+                '30 Templates',
+                '7 Landing Pages',
+                '12 Internal Pages',
+                'Basic Assistance',
               ],
             },
             {
-              name: "Total Developers",
-              price: "$37.99",
-              duration: "Monthly",
-              mainFeature: "Startups",
+              name: 'Total Developers',
+              price: '$37.99',
+              duration: 'Monthly',
+              mainFeature: 'Startups',
               features: [
-                "60 Templates",
-                "15 Landing Pages",
-                "22 Internal Pages",
-                "Priority Assistance",
+                '60 Templates',
+                '15 Landing Pages',
+                '22 Internal Pages',
+                'Priority Assistance',
               ],
               featured: true,
             },
             {
-              name: "To be allocated",
-              price: "$57.99",
-              duration: "Monthly",
-              mainFeature: "Enterprises",
+              name: 'To be allocated',
+              price: '$57.99',
+              duration: 'Monthly',
+              mainFeature: 'Enterprises',
               features: [
-                "90 Templates",
-                "27 Landing Pages",
-                "37 Internal Pages",
-                "Personal Assistance",
+                '90 Templates',
+                '27 Landing Pages',
+                '37 Internal Pages',
+                'Personal Assistance',
               ],
             },
           ]}
@@ -190,6 +190,33 @@ const SaaSProductLandingPage = () => {
           },
         ]}
       /> */}
+
+        <div style={{ width: '100%', textAlign: 'center', marginTop: 60 }}>
+          <img
+            // src={CollabIcon.src}
+            alt={'Collaboration Icon'}
+            className="img-fluid"
+          />
+        </div>
+
+        <div id="synergy">
+          <div className="container">
+            <div className="main_heading text-center">
+              <h3>The Synergy of White Label Technologies with Innow8 Apps</h3>
+              <p>
+                Since 2017, Innow8 Apps has made an impact across the globe by
+                delivering custom web & mobile apps along with services for
+                digital marketing and expert consultation. White Label
+                Technologies acts an arm for Innow8 Apps and has emerged as the
+                brand representing this alliance in their Canada headquarters.
+                Striving together with the same mission, Innow8 Apps and White
+                Label are under the same ownership umbrella to create an impact
+                Worldwide.
+              </p>
+            </div>
+          </div>
+        </div>
+
         <FAQ
           subheading={
             <span className="uppercase tracking-widest font-bold text-primary-500">
@@ -203,24 +230,24 @@ const SaaSProductLandingPage = () => {
           }
           faqs={[
             {
-              question: "What are the technologies that you use ?",
+              question: 'What are the technologies that you use ?',
               answer:
-                "At Whiten App Solutions we continuously update our tech stack to meet the demand of our clients. Presently we work with technologies including - React, React Native, Flutter, Angular, Golang, Nodejs, Swift, Java, Android, Power BI, UI/UX, Quality Assurance, MongoDB",
+                'At Whiten App Solutions we continuously update our tech stack to meet the demand of our clients. Presently we work with technologies including - React, React Native, Flutter, Angular, Golang, Nodejs, Swift, Java, Android, Power BI, UI/UX, Quality Assurance, MongoDB',
             },
             {
-              question: "How Does your Pricing work?",
+              question: 'How Does your Pricing work?',
               answer:
-                "All our solutions and services are 100% custom and are built according to the need of our clients. The pricing of each solution varies as its purely dependent on the demands and feature requirements.",
+                'All our solutions and services are 100% custom and are built according to the need of our clients. The pricing of each solution varies as its purely dependent on the demands and feature requirements.',
             },
             {
-              question: "Can I hire only one developer?",
+              question: 'Can I hire only one developer?',
               answer:
-                "Yes, If you already have a team and are behind a developer, you can hire our top developers to work with your team. We provide contract hiring services to help clients finish their projects quickly by not making commitments and avoiding permanent hires for temporary needs.",
+                'Yes, If you already have a team and are behind a developer, you can hire our top developers to work with your team. We provide contract hiring services to help clients finish their projects quickly by not making commitments and avoiding permanent hires for temporary needs.',
             },
             {
-              question: "What time zone do we follow?",
+              question: 'What time zone do we follow?',
               answer:
-                "Our clients are all around the globe and thereby each project is handled carefully by assigning a team who works on our clients timeline to avoid communication gaps and to be well synchronized with your team.",
+                'Our clients are all around the globe and thereby each project is handled carefully by assigning a team who works on our clients timeline to avoid communication gaps and to be well synchronized with your team.',
             },
           ]}
         />
