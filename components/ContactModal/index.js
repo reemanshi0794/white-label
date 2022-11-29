baa08136992e49fedcdf781adea0d83543b61238
Sana <sana@innow8apps.com>
2022-11-29 15:23:01 +0530

    fix: blog page and main modal ui

--- components/ContactModal/index.js
+++ components/ContactModal/index.js
@@ -1,110 +1,110 @@
-import React, { useState } from 'react';
-import ContactUsSrc from '../../assets/images/contact-us.png';
-import { PrimaryButton as PrimaryButtonBase } from '../../components/misc/Buttons.js';
-import Cross from '../../assets/images/Cross.png';
+import React, { useState } from "react"
+import ContactUsSrc from "../../assets/images/contact-us.png"
+import { PrimaryButton as PrimaryButtonBase } from "../../components/misc/Buttons.js"
+import Cross from "../../assets/images/Cross.png"
 
 function ContactModal({
-  subheading = 'Contact Us Feel free to get in touch with us.',
+  subheading = "Contact Us Feel free to get in touch with us.",
   heading = <></>,
   description = "Let's collaborate",
-  submitButtonText = 'Send',
+  submitButtonText = "Send",
   setShow,
 }) {
-  const handleClose = () => setShow(false);
-  const webDevelopment = 'Web Development';
-  const [contactInfo, setContactInfo] = useState({ service: webDevelopment });
-  const [showLoader, setShowLoader] = useState(false);
+  const handleClose = () => setShow(false)
+  const webDevelopment = "Web Development"
+  const [contactInfo, setContactInfo] = useState({ service: webDevelopment })
+  const [showLoader, setShowLoader] = useState(false)
   const [displayMessage, setDisplayMessage] = useState({
-    message: '',
-    type: '',
-  });
+    message: "",
+    type: "",
+  })
 
   const handleChange = (key, value) => {
-    setContactInfo({ ...contactInfo, [key]: value });
-  };
+    setContactInfo({ ...contactInfo, [key]: value })
+  }
 
   function sendMail() {
     return new Promise((resolve, reject) => {
       try {
-        if (!contactInfo) return;
-        fetch('https://email-innow8.herokuapp.com/email', {
-          method: 'POST',
+        if (!contactInfo) return
+        fetch("https://email-innow8.herokuapp.com/email", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(
             {
-              projectName: 'Whiten-App Solutions',
-              to: ['kapilbindal1@gmail.com'],
-              from: 'contact@innow8apps.com',
-              subject: 'contact form Whiten-App Solutions',
+              projectName: "Whiten-App Solutions",
+              to: ["kapilbindal1@gmail.com"],
+              from: "contact@innow8apps.com",
+              subject: "contact form Whiten-App Solutions",
               text: JSON.stringify(contactInfo),
               htmlText: JSON.stringify(contactInfo),
-            } || ''
+            } || ""
           ),
         })
           .then((res) => {
-            console.log('result: ', res);
-            resolve();
+            console.log("result: ", res)
+            resolve()
           })
           .catch((error) => {
-            console.log('send mail api error', error);
-            reject();
-          });
+            console.log("send mail api error", error)
+            reject()
+          })
       } catch (error) {
-        console.log('send mail try catch error', error);
-        reject();
+        console.log("send mail try catch error", error)
+        reject()
       }
-    });
+    })
   }
 
   const addContactInfo = async () => {
-    return sendMail();
-  };
+    return sendMail()
+  }
 
   const emptyFieldValidationCheck =
     !contactInfo.name ||
     !contactInfo.email ||
     !contactInfo.phoneNumber ||
-    !contactInfo.service;
+    !contactInfo.service
   const handleSubmit = (event) => {
-    setShowLoader(true);
+    setShowLoader(true)
     var validRegex =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     if (contactInfo.email.match(validRegex)) {
       addContactInfo()
         .then((res) => {
           setDisplayMessage({
-            message: 'Submitted successfully',
-            type: 'success',
-          });
-          setShowLoader(false);
-          setTimeout(() => setDisplayMessage({ message: '', type: '' }), 2000);
+            message: "Submitted successfully",
+            type: "success",
+          })
+          setShowLoader(false)
+          setTimeout(() => setDisplayMessage({ message: "", type: "" }), 2000)
         })
         .catch((err) => {
           setDisplayMessage({
-            message: 'An error occurred',
-            type: 'error',
-          });
-          setShowLoader(false);
-          setTimeout(() => setDisplayMessage({ message: '', type: '' }), 2000);
-        });
+            message: "An error occurred",
+            type: "error",
+          })
+          setShowLoader(false)
+          setTimeout(() => setDisplayMessage({ message: "", type: "" }), 2000)
+        })
     } else {
       setDisplayMessage({
-        message: ' Invalid Email ',
-        type: 'error',
-      });
-      setShowLoader(false);
+        message: " Invalid Email ",
+        type: "error",
+      })
+      setShowLoader(false)
     }
-    setContactInfo({ service: webDevelopment });
-    event.preventDefault();
-  };
+    setContactInfo({ service: webDevelopment })
+    event.preventDefault()
+  }
 
   return (
     <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 bg-[#56565699] z-50 p-12 w-full md:inset-0 h-full justify-center items-center flex">
-      <div className=" w-auto lg:max-w-[816px] 2xl:max-w-[1170px] my-auto mx-auto relative ">
+      <div className=" w-auto lg:max-w-[704px] 2xl:max-w-[1170px] my-auto mx-auto relative flex items-center h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex justify-end p-[14px]">
+          <div className="flex justify-end p-1 2xl:p-[14px] ">
             <button onClick={handleClose} className="cursor-pointer">
               <img
                 src={Cross.src}
@@ -112,42 +112,44 @@ function ContactModal({
               />
             </button>
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 pt-[10px] p-12 items-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2 px-6 2xl:px-12 pt-0 py-6 2xl:py-6 items-center">
             <div className="flex-shrink-0">
               <img
                 src={ContactUsSrc.src}
                 alt="contact-us"
-                className="w-[67%] m-auto lg:w-[94%] 2xl:w-full"
+                className="w-[67%] m-auto lg:w-[80%] 2xl:w-full"
               />
             </div>
-            <div className={` md:mx-0 mt-12 md:mt-0 lg:mr-16 lg:order-first`}>
+            <div
+              className={` md:mx-0 mt-12 md:mt-0 mr-4 2xl:mr-16 lg:order-first`}
+            >
               <div className="text-center md:text-left">
                 {subheading && (
-                  <h5 className="font-bold text-[26px] 2xl:text-3xl text-primary-500 text-center lg:text-left">
+                  <h5 className="font-bold text-[16px] 2xl:text-3xl text-primary-500 text-center lg:text-left">
                     Feel free to get in touch with us.
                   </h5>
                 )}
                 {description && (
-                  <p className="mt-4 text-center lg:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100">
+                  <p className=" mt-2 2xl:mt-4 text-center lg:text-left text-sm md:text-base 2xl:text-lg font-medium leading-relaxed text-secondary-100">
                     {description}
                   </p>
                 )}
                 <form
                   action=""
                   method="post"
-                  className="mt-4 md:mt-8 2xl:mt-10 text-sm flex flex-col lg:max-w-sm mx-auto md:mx-0"
+                  className="mt-4 2xl:mt-10 text-sm flex flex-col lg:max-w-xs 2xl:max-w-sm mx-auto md:mx-0"
                 >
                   <input
                     type="text"
                     name="name"
-                    value={contactInfo.name || ''}
+                    value={contactInfo.name || ""}
                     placeholder="Full Name"
-                    className="mt-6 first:mt-0 border-2 px-4 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
+                    className="mt-6 first:mt-0 border-2 px-3 py-2 2xl:px-4 2xl:py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
                     id="exampleInputEmail1"
                     required
                     aria-describedby="emailHelp"
                     onChange={(event) =>
-                      handleChange('name', event.target.value)
+                      handleChange("name", event.target.value)
                     }
                     autoComplete="off"
                   />
@@ -155,13 +157,13 @@ function ContactModal({
                     type="email"
                     name="email"
                     required
-                    value={contactInfo.email || ''}
+                    value={contactInfo.email || ""}
                     placeholder="Email Address"
-                    className="mt-6 first:mt-0 border-2 px-4 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
+                    className="mt-3 2xl:mt-6 first:mt-0 border-2 px-3 py-2 2xl:px-4 2xl:py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={(event) =>
-                      handleChange('email', event.target.value)
+                      handleChange("email", event.target.value)
                     }
                     autoComplete="off"
                   />
@@ -169,29 +171,29 @@ function ContactModal({
                     type="tel"
                     name="phoneNumber"
                     required
-                    value={contactInfo.phoneNumber || ''}
+                    value={contactInfo.phoneNumber || ""}
                     placeholder="Phone Number"
-                    className="mt-6 first:mt-0 border-2 px-4 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
+                    className="mt-3 2xl:mt-6 first:mt-0 border-2 px-3 py-2 2xl:px-4 2xl:py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={(event) =>
                       handleChange(
-                        'phoneNumber',
+                        "phoneNumber",
                         event.target.value >= 0 ? event.target.value : 0
                       )
                     }
                     autoComplete="off"
                   />
                   <textarea
-                    className="mt-6 first:mt-0 border-2 px-4 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
+                    className="mt-3 2xl:mt-6 first:mt-0 border-2 px-3 py-2 2xl:px-4 2xl:py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500"
                     name="message"
                     required
-                    value={contactInfo.message || ''}
+                    value={contactInfo.message || ""}
                     placeholder="Message"
                     id="exampleFormControlTextarea1"
                     rows="2"
                     onChange={(event) =>
-                      handleChange('message', event.target.value)
+                      handleChange("message", event.target.value)
                     }
                   />
                   {showLoader ? (
@@ -212,9 +214,9 @@ function ContactModal({
                     <div className={`message-${displayMessage.type}`}>
                       <span
                         className={
-                          displayMessage.message === 'Submitted successfully'
-                            ? 'text-green-600'
-                            : 'text-red-600'
+                          displayMessage.message === "Submitted successfully"
+                            ? "text-green-600"
+                            : "text-red-600"
                         }
                       >
                         {displayMessage.message}
@@ -223,7 +225,7 @@ function ContactModal({
                   )}
                   <div>
                     <PrimaryButtonBase
-                      className="inline-block mt-8 !rounded-full"
+                      className="inline-block !px-[24px] py-[8px] 2xl:!px-8 !2xl:py-3 mt-4 2xl:mt-8 !rounded-full"
                       type="submit"
                       onClick={handleSubmit}
                       disabled={emptyFieldValidationCheck}
@@ -238,7 +240,7 @@ function ContactModal({
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ContactModal;
+export default ContactModal
--- pages/blog-post.js
+++ pages/blog-post.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import Header from '../components/headers/light.js';
-import Footer from '../components/footers/FiveColumnWithInputForm.js';
-import Crowdfunding from '../assets/images/Crowdfunding.png';
-import Nft from '../assets/images/Nft.png';
-import Head from 'next/head';
+import React, { useState } from "react"
+import Header from "../components/headers/light.js"
+import Footer from "../components/footers/FiveColumnWithInputForm.js"
+import Crowdfunding from "../assets/images/Crowdfunding.png"
+import Nft from "../assets/images/Nft.png"
+import Head from "next/head"
 
 export default ({
-  headingText = 'Blog Posts',
+  headingText = "Blog Posts",
   posts = [
     {
       imageSrc: Crowdfunding,
-      date: 'November 28, 2022',
-      title: 'A complete guide for developing Crowdfunding App in 2022',
+      date: "November 28, 2022",
+      title: "A complete guide for developing Crowdfunding App in 2022",
       description:
-        'In the digital economy, crowdfunding is gaining popularity and rising to the top. The size of the worldwide crowdfunding business was over $17 billion in 2021, and by 2028, it is expected to be worth over $43 billion....',
-      url: 'https://reddit.com',
+        "In the digital economy, crowdfunding is gaining popularity and rising to the top. The size of the worldwide crowdfunding business was over $17 billion in 2021, and by 2028, it is expected to be worth over $43 billion....",
+      url: "https://reddit.com",
     },
     {
       imageSrc: Nft,
-      date: 'November 28, 2022',
-      title: 'Development of a Whiten Apps NFT Marketplace',
+      date: "November 28, 2022",
+      title: "Development of a Whiten Apps NFT Marketplace",
       description:
-        'The building of the NFT marketplace can use ready-made solutions called NFT Marketplace Clone solutions.It entails copying any well-known NFT marketplace, such as Opensea or Rarible, and implementing UI platform adjustments based on the needs of the company...',
-      url: 'https://reddit.com',
+        "The building of the NFT marketplace can use ready-made solutions called NFT Marketplace Clone solutions.It entails copying any well-known NFT marketplace, such as Opensea or Rarible, and implementing UI platform adjustments based on the needs of the company...",
+      url: "https://reddit.com",
     },
     // getPlaceholderPost(),
     // getPlaceholderPost(),
@@ -43,10 +43,10 @@ export default ({
     // getPlaceholderPost(),
   ],
 }) => {
-  const [visible, setVisible] = useState(7);
+  const [visible, setVisible] = useState(7)
   const onLoadMoreClick = () => {
-    setVisible((v) => v + 6);
-  };
+    setVisible((v) => v + 6)
+  }
 
   return (
     <>
@@ -66,108 +66,108 @@ export default ({
           />
         </Head>
         <Header />
-        <div className="relative">
-          <div className="max-w-screen-xl mx-auto pb-24 pt-[11rem]">
-            <div className="flex">
-              <h2 className="text-4xl sm:text-5xl font-black tracking-wide text-center text-gray-900">
-                {headingText}
-              </h2>
-            </div>
-            <div className="mt-6 sm:-mr-8 flex flex-wrap">
-              {posts.slice(0, visible).map((post, index) => {
-                return (
-                  <div
-                    className={
-                      post.featured
-                        ? 'w-full! sm:pr-8'
-                        : 'mt-10 w-full sm:w-1/2 lg:w-1/3 sm:pr-8'
-                    }
-                    key={index}
-                    featured={post.featured}
-                  >
-                    <a
-                      className={
-                        post.featured
-                          ? 'sm:flex-row! h-full sm:pr-4 group flex bg-gray-100 rounded-lg'
-                          : 'group cursor-pointer flex flex-col bg-gray-100 rounded-lg'
-                      }
-                      as="a"
-                      href={post.url}
+        <div className="min-h-screen text-secondary-500 p-8 lg:px-24 2xl:p-8 overflow-hidden">
+          <div className="relative">
+            <div className="max-w-screen-xl mx-auto pb-24 pt-[11rem]">
+              <div className="flex">
+                <h2 className="text-4xl sm:text-5xl font-black tracking-wide text-center text-gray-900">
+                  {headingText}
+                </h2>
+              </div>
+              <div className="mt-6 sm:-mr-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
+                {posts.slice(0, visible).map((post, index) => {
+                  return (
+                    <div
+                      className={post.featured ? " sm:pr-8" : "mt-10 sm:pr-8"}
+                      key={index}
+                      featured={post.featured}
                     >
-                      <div
+                      <a
                         className={
                           post.featured
-                            ? 'sm:h-96 sm:min-h-full sm:w-1/2 lg:w-2/3 sm:rounded-t-none sm:rounded-l-lg xl:w-8/12 h-64 w-full bg-cover bg-center rounded-t-lg'
-                            : 'h-64 w-full bg-cover bg-center rounded-t-lg'
+                            ? "sm:flex-row! h-full sm:pr-4 group flex bg-gray-100 rounded-lg"
+                            : "group cursor-pointer flex flex-col bg-gray-100 rounded-lg"
                         }
+                        as="a"
+                        href={post.url}
                       >
-                        <img
-                          src={post.imageSrc.src}
+                        <div
                           className={
-                            post.featured ? 'w-full object-cover h-96' : ''
+                            post.featured
+                              ? "sm:h-96 sm:min-h-full sm:w-1/2 lg:w-2/3 sm:rounded-t-none sm:rounded-l-lg xl:w-8/12 h-64 w-full bg-cover bg-center rounded-t-lg"
+                              : "h-64 w-full bg-cover bg-center rounded-t-lg"
                           }
-                        />
-                      </div>
-                      <div
-                        className={
-                          post.featured
-                            ? 'sm:-mr-4 sm:pl-8 sm:flex-1 sm:rounded-none sm:rounded-r-lg sm:border-t-2 sm:border-l-0 p-8 border-2 border-t-0 rounded-lg rounded-t-none'
-                            : 'p-8 border-2 border-t-0 rounded-lg rounded-t-none'
-                        }
-                      >
-                        <div className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose after:content after:block after:border-b-2 after:border-primary-500 after:w-8">
-                          {post.category}
-                        </div>
-                        <div className="mt-4 uppercase text-gray-600 italic font-semibold text-xs">
-                          {post.date}
-                        </div>
-                        <div className="mt-1 font-black text-2xl text-gray-900 group-hover:text-primary-500 transition duration-300">
-                          {post.title}
-                        </div>
-                        {post.description && (
-                          <div
+                        >
+                          <img
+                            src={post.imageSrc.src}
                             className={
                               post.featured
-                                ? 'text-sm mt-3 leading-loose text-gray-600 font-medium '
-                                : ''
+                                ? "object-cover w-full h-full"
+                                : "w-full h-full"
                             }
-                          >
-                            {post.description}
+                          />
+                        </div>
+                        <div
+                          className={
+                            post.featured
+                              ? "sm:-mr-4 sm:pl-8 sm:flex-1 sm:rounded-none sm:rounded-r-lg sm:border-t-2 sm:border-l-0 p-8 border-2 border-t-0 rounded-lg rounded-t-none"
+                              : "p-8 border-2 border-t-0 rounded-lg rounded-t-none"
+                          }
+                        >
+                          <div className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose after:content after:block after:border-b-2 after:border-primary-500 after:w-8">
+                            {post.category}
                           </div>
-                        )}
-                      </div>
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-            {/* {visible < posts.length && (
-              <div className="flex justify-center">
-                <button
-                  className="mt-16 mx-auto px-8 py-3 font-bold rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300"
-                  onClick={onLoadMoreClick}
-                >
-                  Load More
-                </button>
+                          <div className="mt-4 uppercase text-gray-600 italic font-semibold text-xs">
+                            {post.date}
+                          </div>
+                          <div className="mt-1 font-black text-2xl text-gray-900 group-hover:text-primary-500 transition duration-300">
+                            {post.title}
+                          </div>
+                          {post.description && (
+                            <div
+                              className={
+                                post.featured
+                                  ? "text-sm mt-3 leading-loose text-gray-600 font-medium "
+                                  : ""
+                              }
+                            >
+                              {post.description}
+                            </div>
+                          )}
+                        </div>
+                      </a>
+                    </div>
+                  )
+                })}
               </div>
-            )} */}
+              {/* {visible < posts.length && (
+                <div className="flex justify-center">
+                  <button
+                    className="mt-16 mx-auto px-8 py-3 font-bold rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300"
+                    onClick={onLoadMoreClick}
+                  >
+                    Load More
+                  </button>
+                </div>
+              )} */}
+            </div>
           </div>
         </div>
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
 const getPlaceholderPost = () => ({
   imageSrc:
-    'https://images.unsplash.com/photo-1418854982207-12f710b74003?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80',
-  date: 'November 28, 2022',
-  title: 'A complete guide for developing Crowdfunding App in 2022',
+    "https://images.unsplash.com/photo-1418854982207-12f710b74003?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80",
+  date: "November 28, 2022",
+  title: "A complete guide for developing Crowdfunding App in 2022",
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  url: 'https://reddit.com',
-});
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  url: "https://reddit.com",
+})
 // {
 //   imageSrc:
 //     'https://images.unsplash.com/photo-1418854982207-12f710b74003?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80',
