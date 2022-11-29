86792ae2027d48d18995c8f247e96e48a8d5b33d
Reemanshi <reema07chugh@gmail.com>
2022-11-29 17:38:09 +0530

    fix: header in both the blogs

--- pages/A-complete-guide-for-developing-crowdfunding-app-in-2022.js
+++ pages/A-complete-guide-for-developing-crowdfunding-app-in-2022.js
@@ -4,6 +4,8 @@ import Title from '../components/blog/Title';
 import Description from '../components/blog/Description';
 import Image from '../components/blog/Image';
 import { CrowdFundingAppData } from '../helpers/utils';
+import Header from '../components/headers/light.js';
+import Head from 'next/head';
 
 const Howmuchcosttodevelop = () => {
   useEffect(() => {
@@ -23,30 +25,49 @@ const Howmuchcosttodevelop = () => {
   };
 
   return (
-    <div className="min-h-screen text-secondary-500 p-8 lg:px-24 2xl:p-8 overflow-hidden">
-      <div className="inner-sec">
-        <div
-          className="banner size"
-          style={{
-            // backgroundImage: `url(${CostDevelopBanner.src})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
-        >
-          <div className="container">
-            <div className="row m-0">
-              <div className="col">
-                <h1 className="text-[#151514] text-[60px] font-bold mb-10">
-                  A complete guide for developing Crowdfunding App in 2022
-                </h1>
+    <div>
+      <Head>
+        <title>
+          Software Development Company | Custom App Developer | White Label
+          Technologies
+        </title>
+        <meta
+          name="description"
+          content="Design your apps and website with top software development company at reasonable prices. Experienced custom app designers and developers. Contact us today!"
+        />
+        <meta
+          name="keyword"
+          content="custom mobile app development company, software development company,custom software development,software app developer"
+        />
+      </Head>
+      <Header />
+      <div className="relative">
+        <div className="max-w-screen-xl mx-auto pb-24 pt-[11rem]">
+          <div className="inner-sec">
+            <div
+              className="banner size"
+              style={{
+                // backgroundImage: `url(${CostDevelopBanner.src})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="container">
+                <div className="row m-0">
+                  <div className="col">
+                    <h1 className="text-[#151514] text-[60px] font-bold mb-10">
+                      A complete guide for developing Crowdfunding App in 2022
+                    </h1>
+                  </div>
+                </div>
               </div>
             </div>
+            {CrowdFundingAppData.map(({ type, content, className }) => (
+              <div className={className}>{getComponent(type, content)}</div>
+            ))}
           </div>
         </div>
-        {CrowdFundingAppData.map(({ type, content, className }) => (
-          <div className={className}>{getComponent(type, content)}</div>
-        ))}
       </div>
     </div>
   );
--- pages/Development-of-a-whiten-apps-nft-marketplace-2022.js
+++ pages/Development-of-a-whiten-apps-nft-marketplace-2022.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import Header from '../components/headers/light.js';
 import { NFTMarketplace } from '../helpers/utils';
 import Title from '../components/blog/Title';
 import Description from '../components/blog/Description';
 import Image from '../components/blog/Image';
+import Head from 'next/head';
 
 const NftMarketplace = () => {
   useEffect(() => {
@@ -23,30 +25,50 @@ const NftMarketplace = () => {
   };
 
   return (
-    <div className="min-h-screen text-secondary-500 p-8 lg:px-24 2xl:p-8 overflow-hidden">
-      <div className="inner-sec">
-        <div
-          className="banner size"
-          style={{
-            // backgroundImage: `url(${CostDevelopBanner.src})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
-        >
-          <div className="container">
-            <div className="row m-0">
-              <div className="col">
-                {/* <h1 className="text-[#151514] text-[60px] font-bold mb-10">
+    <div>
+      <Head>
+        <title>
+          Software Development Company | Custom App Developer | White Label
+          Technologies
+        </title>
+        <meta
+          name="description"
+          content="Design your apps and website with top software development company at reasonable prices. Experienced custom app designers and developers. Contact us today!"
+        />
+        <meta
+          name="keyword"
+          content="custom mobile app development company, software development company,custom software development,software app developer"
+        />
+      </Head>
+      <Header />
+      <br />
+      <div className="relative">
+        <div className="max-w-screen-xl mx-auto pb-24 pt-[11rem]">
+          <div className="inner-sec">
+            <div
+              className="banner size"
+              style={{
+                // backgroundImage: `url(${CostDevelopBanner.src})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="container">
+                <div className="row m-0">
+                  <div className="col">
+                    {/* <h1 className="text-[#151514] text-[60px] font-bold mb-10">
                   A complete guide for developing Crowdfunding App in 2022
                 </h1> */}
+                  </div>
+                </div>
               </div>
             </div>
+            {NFTMarketplace.map(({ type, content, className }) => (
+              <div className={className}>{getComponent(type, content)}</div>
+            ))}
           </div>
         </div>
-        {NFTMarketplace.map(({ type, content, className }) => (
-          <div className={className}>{getComponent(type, content)}</div>
-        ))}
       </div>
     </div>
   );
