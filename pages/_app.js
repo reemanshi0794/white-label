1e9c213357ba2ae5716bd2a9ff654aa9d70c73d8
Reemanshi <reema07chugh@gmail.com>
2022-12-07 14:40:14 +0530

    fix: added gtags,metadata, canonical link

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
+import Head from 'next/head';
+import {useRouter} from 'next/router';
+import {GlobalStyles} from 'twin.macro';
 import '../styles/globals.css';
-import { GlobalStyles } from 'twin.macro';
-import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
+  const router = useRouter();
+  const canonicalUrl = (`https://whitenappsolutions.com` + (router.asPath === '/' ? '' : router.asPath)).split('?')[0];
+
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-773YZ1L3YR"></script>
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-773YZ1L3YR');
+                });
+              `,
+          }}
+        />
+      </Head>
       <div>
         <GlobalStyles />
         <Component {...pageProps} />
