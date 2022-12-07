eed9a1578d297693393162a52541977b595c4267
Reemanshi <reema07chugh@gmail.com>
2022-12-07 11:42:49 +0530

    fix: blog page ui

--- components/blog/Image.js
+++ components/blog/Image.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react"
 
 const Image = ({ content }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 mb-4 md:mb-8 lg:mb-20">
-      <div>
-        <img src={content.src} alt="post1" className="h-full object-cover" />
-      </div>
+    <div className="mb-4 md:mb-8 lg:mb-20">
+      <img src={content.src} alt="post1" className="h-full object-cover" />
     </div>
-  );
-};
+  )
+}
 
-export default Image;
+export default Image
