12d708c34121f1493880068da49432906dec98d6
Reemanshi <reema07chugh@gmail.com>
2022-12-09 11:48:19 +0530

    fix: portfollio page ui

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { PortfolioData } from '../helpers/utils';
-import Head from 'next/head';
-import Header from '../components/headers/light.js';
-import Footer from '../components/footers/FiveColumnWithInputForm.js';
+import React from "react"
+import { PortfolioData } from "../helpers/utils"
+import Head from "next/head"
+import Header from "../components/headers/light.js"
+import Footer from "../components/footers/FiveColumnWithInputForm.js"
 
 const portfolio = () => {
   return (
@@ -22,104 +22,100 @@ const portfolio = () => {
         />
       </Head>
       <Header />
-      <div className="who-we-are-section">
-        <div className="container-fluid">
-          <div className="col-lg-10 col-md-11 mx-auto">
-            <div className="main_heading text-center">
-              <h4>Our Portfolio</h4>
-            </div>
-            <div className="row">
-              {PortfolioData.map((item, index) => {
-                const { id, logo, url, type, message } = item;
-                return (
-                  <>
-                    {index % 2 === 0 ? (
-                      <>
-                        <div className="col-md-6 col-12 mb-4" key={id}>
-                          <div className="img_section ">
-                            {type.toLowerCase().includes('img') ? (
-                              <div className="img-wrapper">
-                                {/* <Loader /> */}
-                                <img
-                                  className="img-fluid"
-                                  src={url.src}
-                                  alt={url}
-                                />
-                              </div>
-                            ) : (
-                              <>
-                                {/* <Loader /> */}
-                                <video
-                                  id="video-id-name"
-                                  src={url}
-                                  // controls
-                                  loop
-                                  autoPlay
-                                  muted
-                                />
-                              </>
-                            )}
-                          </div>
-                        </div>
-                        <div className="col-md-6 col-12 mb-4">
-                          <div className="content_section">
-                            <div className="logo-wrapper">
-                              <img src={logo.src} alt={logo} />
-                            </div>
-                            <p>{message}</p>
-                          </div>
-                        </div>
-                      </>
-                    ) : (
-                      <>
-                        <div className="row orderChange mb-4">
-                          <div className="col-md-6 col-12">
-                            <div className="content_section">
-                              <div className="logo-wrapper">
-                                <img src={logo.src} alt={logo} />
-                              </div>
-                              <p>{message}</p>
-                            </div>
-                          </div>
-                          <div className="col-md-6 col-12">
-                            <div className="img_section ">
-                              {url.toLowerCase().includes('img') ? (
-                                <div className="img-wrapper">
-                                  {/* <Loader /> */}
-                                  <img
-                                    className="img-fluid"
-                                    src={url}
-                                    alt={url}
-                                  />
-                                </div>
-                              ) : (
-                                <>
-                                  {/* <Loader /> */}
-                                  <video
-                                    id="video-id-name"
-                                    src={url}
-                                    // controls
-                                    loop
-                                    autoPlay
-                                    muted
-                                  />
-                                </>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </>
-                    )}
-                  </>
-                );
-              })}
-            </div>
+      <div className="min-h-screen text-secondary-500 p-8 lg:px-24 2xl:p-8 overflow-hidden">
+        <div className="max-w-screen-xl mx-auto mt-40">
+          <div className="text-center pb-[60px]">
+            <h4 className=" text-2xl">Our Portfolio</h4>
           </div>
+          {PortfolioData.map((item, index) => {
+            const { id, logo, url, type, message } = item
+            return (
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center mb-4">
+                <>
+                  {index % 2 === 0 ? (
+                    <>
+                      <div
+                        className=" border-[#747474] border p-[10px] rounded-[20px]"
+                        key={id}
+                      >
+                        {type.toLowerCase().includes("img") ? (
+                          <div>
+                            {/* <Loader /> */}
+                            <img
+                              src={url.src}
+                              alt={url}
+                              className="rounded-[20px] object-cover w-full"
+                            />
+                          </div>
+                        ) : (
+                          <>
+                            {/* <Loader /> */}
+                            <video
+                              id="video-id-name"
+                              src={url}
+                              // controls
+                              loop
+                              autoPlay
+                              muted
+                            />
+                          </>
+                        )}
+                      </div>
+
+                      <div className="flex flex-col items-start h-full justify-center">
+                        <img src={logo.src} alt={logo} />
+                        <p className="text-xl text-[#363636] leading-7 py-[26px]">
+                          {message}
+                        </p>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="flex flex-col items-start h-full justify-center">
+                        <img src={logo.src} alt={logo} />
+                        <p className="text-xl text-[#363636] leading-7 py-[26px]">
+                          {message}
+                        </p>
+                      </div>
+
+                      <div>
+                        <div className=" border-[#747474] border p-[10px] rounded-[20px]">
+                          {url.toLowerCase().includes("img") ? (
+                            <div>
+                              {/* <Loader /> */}
+                              <img
+                                src={url}
+                                alt={url}
+                                className="rounded-[20px] object-cover w-full"
+                              />
+                            </div>
+                          ) : (
+                            <>
+                              {/* <Loader /> */}
+                              <video
+                                id="https://www.w3schools.com/tags/movie.mp4"
+                                src={url}
+                                // controls
+
+                                loop
+                                autoPlay
+                                muted
+                              />
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </>
+              </div>
+            )
+          })}
         </div>
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default portfolio;
+export default portfolio
