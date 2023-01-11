12775cc588a669ef4381064b83faed1652f31bf5
KAPIL BINDAL <reema07chugh@gmail.com>
2023-01-11 17:42:44 +0530

    fix: schema updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,17 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { GlobalStyles } from 'twin.macro';
-import '../styles/globals.css';
+import Head from "next/head";
+import {useRouter} from "next/router";
+import {GlobalStyles} from "twin.macro";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   const router = useRouter();
-  const canonicalUrl = (
-    `https://whitenappsolutions.com` +
-    (router.asPath === '/' ? '' : router.asPath)
-  ).split('?')[0];
+  const canonicalUrl = (`https://whitenappsolutions.com` + (router.asPath === "/" ? "" : router.asPath)).split("?")[0];
 
   return (
     <>
       <Head>
         <link rel="canonical" href={canonicalUrl} />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-773YZ1L3YR"
-        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-773YZ1L3YR" />
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -29,6 +23,30 @@ function MyApp({ Component, pageProps }) {
               `,
           }}
         />
+        <script type="application/ld+json">
+          {`{
+  "@context": "https://schema.org",
+  "@type": "Corporation",
+  "name": "Whiten App Solutions",
+  "url": "https://whitenappsolutions.com/",
+  "logo": "",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+1 (416) 948-7063",
+    "contactType": "customer service",
+    "areaServed": "CA",
+    "availableLanguage": "en"
+  },
+  "sameAs": [
+    "https://www.facebook.com/profile.php?id=100088241919758",
+    "https://whitenappsolutions.com/",
+    "https://www.linkedin.com/company/whiten-app-solutions/",
+    "https://www.instagram.com/whiten_apps/",
+    "https://twitter.com/WhitenApps",
+    "https://www.youtube.com/@whitenappssolutions"
+  ]
+}`}
+        </script>
       </Head>
       <div>
         <GlobalStyles />
